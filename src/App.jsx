dc1be6299eae319eab252082699876e392456a36
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-01-21 08:52:04 +0100

    add

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Nav from "./Navbar";
+import Main from "./MainComp";
+
+export function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
---
+++ src/MainComp.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+  return (
+    <div className="main-content">
+      <h1> Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 20123</li>
+        <li> Was originally created by Jorand Walke </li>
+        <li> Has well over 10k stars on Github</li>
+        <li>Maintained by facebook</li>
+        <li>Powers thousand of enterprises</li>
+      </ul>
+    </div>
+  );
+}

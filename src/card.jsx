d852057df69643a154e011d2830a87ee23aa36e0
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-01-22 20:38:51 +0100

    add

--- src/card.jsx
+++ src/card.jsx
@@ -1,14 +1,16 @@
 export default function Card(props) {
   return (
-    <div>
+    <div className="container--flex">
       <img src={props.item.imageUrl} />
-      <div>
-        <p> {props.item.location} </p>
-        <a href={props.item.googleMapsUrl}></a>
+      <div className="textbox">
+        <div>
+          <p> {props.item.location} </p>
+          <a href={props.item.googleMapsUrl}></a>
+        </div>
+        <h1>{props.item.title}</h1>
+        <p> {`${props.item.startDate}-${props.item.endDate}`} </p>
+        <p> {props.item.description}</p>
       </div>
-      <h1>{props.item.title}</h1>
-      <p> {`${props.item.startDate}-${props.item.endDate}`} </p>
-      <p> {props.item.description}</p>
     </div>
   );
 }
